55f14902bdff9fac870ce5cd04fa6abb0b9f1262
drools <drools@droolsdeMacBook-Pro.local>
2018-04-10 14:15:30 +0800

    jetty embed log and  login

--- src/main/java/com/goosun/glass/GlassApplication.java
+++ src/main/java/com/goosun/glass/GlassApplication.java
@@ -2,20 +2,12 @@ package com.goosun.glass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class GlassApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(GlassApplication.class, args);
 	}
-	
-	@RequestMapping(value = "/",produces = "text/plain;charset=UTF-8")
-    String index(){
-        return "Hello Spring Boot! ";
-    }
 	
 }
--- src/main/java/com/goosun/glass/app/WebSecurityConfig.java
+++ src/main/java/com/goosun/glass/app/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.goosun.glass.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 import com.goosun.glass.service.impl.CustomUserServiceImpl;
 
@@ -20,6 +22,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return new CustomUserServiceImpl();
 	}
 
+	@Autowired
+	private AccessDeniedHandler accessDeniedHandler;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(customUserService()).passwordEncoder(passwordEncoder());
@@ -27,13 +32,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/home", "/css/**", "/js/**", "/images/**").permitAll().anyRequest()
-				.authenticated().and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+		http.csrf().disable().authorizeRequests().antMatchers("/", "/home", "/about","/webjars/**", "/css/**", "/js/**", "/images/**")
+				.permitAll().antMatchers("/admin/**").hasAnyRole("ADMIN").antMatchers("/user/**").hasAnyRole("USER")
+				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
+				.permitAll().and().exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+
 	}
-	
+
 	@Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(4);
-    }
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 }
